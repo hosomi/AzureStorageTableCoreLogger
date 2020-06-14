532534bc275a293b6875d36c0cd58fe93db04df0
hosomi <kiyotake.hosomi@outlook.jp>
2020-06-14 13:56:17 +0900

    Logger のコンストラクタ追加に伴うテストコード追加。

--- AzureStorageTableCoreLogger.Tests/TestAzureStorageTableCoreLogger.cs
+++ AzureStorageTableCoreLogger.Tests/TestAzureStorageTableCoreLogger.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.Azure.Cosmos.Table;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace AzureStorageTableCoreLogger.Tests
@@ -24,7 +25,30 @@ namespace AzureStorageTableCoreLogger.Tests
                 log.Error(e, "ERROR");
                 log.Error(e);
             }
+        }
 
+        public void TestInstanceCloudTableLogging()
+        {
+            var storageAccount = ConfigrationUtil.GetCloudStorageAccount("UseDevelopmentStorage=true");
+            var cloudTableClient = storageAccount.CreateCloudTableClient();
+            var cloudTableLogging = cloudTableClient.GetTableReference("TestTable");
+            cloudTableLogging.CreateIfNotExistsAsync().Wait();
+
+            Logger log = new Logger(cloudTableLogging, "TestKey");
+
+            log.Debug("TestInstanceCloudTableLogging-DEBUG");
+            log.Info("TestInstanceCloudTableLogging-INFO");
+            try
+            {
+                int a = 0;
+                int b = 0;
+                int c = a / b;
+            }
+            catch (Exception e)
+            {
+                log.Error(e, "TestInstanceCloudTableLogging-ERROR");
+                log.Error(e);
+            }
         }
 
     }
